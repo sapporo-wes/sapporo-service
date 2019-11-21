e8f479da8c7b82d6331ddf7d4813a6945611d4b6
suecharo <suehiro619@gmail.com>
2019-11-21 20:29:07 +0000

    Impliment s3 upload

--- src/app/lib/util.py
+++ src/app/lib/util.py
@@ -20,9 +20,11 @@ RUN_BASE_DIR.mkdir(parents=True, exist_ok=True)
 RUN_ORDER_FILE_NAME = "run_order.yml"
 WORKFLOW_FILE_NAME = "workflow"
 WORKFLOW_PARAMETERS_FILE_NAME = "workflow_parameters"
+UPLOAD_INFO_FILE_NAME = "upload_info"
 STATUS_FILE_NAME = "status.txt"
 PID_INFO_FILE_NAME = "run.pid"
 UPLOAD_URL_FILE_NAME = "upload_url.txt"
+OUTPUT_DIR_NAME = "output"
 STDOUT_FILE_NAME = "stdout.log"
 STDERR_FILE_NAME = "stderr.log"
 RUN_SHELL_STDOUT_FILE_NAME = "run_shell.stdout.log"
@@ -30,7 +32,7 @@ RUN_SHELL_STDERR_FILE_NAME = "run_shell.stderr.log"
 
 RUN_EXECUTION_SCRIPT_PATH = SERVICE_BASE_DIR.joinpath("src/run_workflow.sh")
 
-SUPPORTED_WES_VERSIONS = ["v1.0.0"]
+SUPPORTED_WES_VERSIONS = ["SapporoWES-v1.0.0"]
 
 
 def read_workflow_info():
