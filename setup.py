4c7d044fda341e50d467c8a84133e01516de5944
suecharo <suehiro619@gmail.com>
2021-03-11 16:01:10 +0900

    Fix version number

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read_requirements_txt() -> List[str]:
 
 def main() -> None:
     setup(name="sapporo",
-          version="1.0.10",
+          version="1.0.11",
           description="Implementation of a GA4GH workflow execution " +
                       "service that can easily support various " +
                       "workflow runners.",
