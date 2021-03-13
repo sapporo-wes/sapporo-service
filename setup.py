c29fdb12615345117f90330ccf3783c3ef5f2b0b
suecharo <suehiro619@gmail.com>
2021-03-13 15:59:41 +0900

    Fix package infomation

--- setup.py
+++ setup.py
@@ -1,50 +1,70 @@
 #!/usr/bin/env python3
 # coding: utf-8
 from pathlib import Path
-from typing import List
 
 from setuptools import setup
 
 BASE_DIR: Path = Path(__file__).parent.resolve()
-REQUIREMENTS_TEXT: Path = BASE_DIR.joinpath("requirements.txt")
 LONG_DESCRIPTION: Path = BASE_DIR.joinpath("README.md")
 
-
-def read_requirements_txt() -> List[str]:
-    with REQUIREMENTS_TEXT.open(mode="r") as f:
-        packages: List[str] = \
-            [str(line) for line in f.read().splitlines() if line != ""]
-
-    return packages
-
-
-def main() -> None:
-    setup(
-        name="sapporo",
-        version="1.0.11",
-        description="Implementation of a GA4GH workflow execution " +
-        "service that can easily support various " +
-        "workflow runners.",
-        long_description=LONG_DESCRIPTION.open(mode="r").read(),
-        long_description_content_type="text/markdown",
-        author="DDBJ(DNA Data Bank of Japan)",
-        author_email="t.ohta@nig.ac.jp",
-        url="https://github.com/ddbj/SAPPORO-service",
-        license="Apache2.0",
-        python_requires=">=3.6",
-        platforms="any",
-        include_package_data=True,
-        zip_safe=False,
-        classifiers=["Programming Language :: Python"],
-        packages=["sapporo"],
-        install_requires=read_requirements_txt(),
-        entry_points={
-            "console_scripts": [
-                "sapporo=sapporo.app:main",
-            ]
-        }
-    )
-
-
-if __name__ == "__main__":
-    main()
+setup(
+    name="sapporo",
+    version="1.0.11",
+    description="SAPPORO is a standard implementation conforming to the "
+    "Global Alliance for Genomics and Health (GA4GH) Workflow Execution "
+    "Service (WES) API specification.",
+    long_description=LONG_DESCRIPTION.open(mode="r").read(),
+    long_description_content_type="text/markdown",
+    author="DDBJ(DNA Data Bank of Japan)",
+    author_email="t.ohta@nig.ac.jp",
+    url="https://github.com/ddbj/SAPPORO-service",
+    license="Apache2.0",
+    python_requires=">=3.6",
+    platforms="any",
+    packages=["sapporo"],
+    package_data={
+        "sapporo": [
+            "executable_workflows.json",
+            "executable_workflows.schema.json",
+            "run.sh",
+            "service-info.json",
+            "service-info.schema.json",
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        "Flask",
+        "jsonschema",
+        "requests",
+        "uwsgi",
+    ],
+    tests_require=[
+        "flake8",
+        "isort",
+        "mypy",
+        "pytest",
+        "typing-extensions",
+    ],
+    entry_points={
+        "console_scripts": [
+            "sapporo=sapporo.app:main",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Flask",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+)
