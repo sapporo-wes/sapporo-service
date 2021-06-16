aa4f42b962399e726a6492693336bc0b25e52501
suecharo <suehiro619@gmail.com>
2021-06-16 16:03:17 +0900

    Fix get status test script

--- tests/unit_test/test_post_runs/__init__.py
+++ tests/unit_test/test_post_runs/__init__.py
@@ -15,26 +15,37 @@ SCRIPT_DIR = \
 
 def get_run_id(run_id: str) -> RunLog:
     script_path = SCRIPT_DIR.joinpath("get_run_id.sh")
-    proc = sp.run(shlex.split(f"/bin/bash {str(script_path)} {run_id}"),
-                  stdout=sp.PIPE,
-                  stderr=sp.PIPE,
-                  encoding="utf-8",
-                  env={"SAPPORO_HOST": TEST_HOST,
-                       "SAPPORO_PORT": TEST_PORT})
-    assert proc.returncode == 0
+    retry = 0
+    while retry < 3:
+        proc = sp.run(shlex.split(f"/bin/bash {str(script_path)} {run_id}"),
+                      stdout=sp.PIPE,
+                      stderr=sp.PIPE,
+                      encoding="utf-8",
+                      env={"SAPPORO_HOST": TEST_HOST,
+                           "SAPPORO_PORT": TEST_PORT})
+        if proc.returncode == 0:
+            break
+        else:
+            retry += 1
     res_data: RunLog = json.loads(proc.stdout)
-
+    assert proc.returncode == 0
     return res_data
 
 
 def get_run_id_status(run_id: str) -> RunStatus:
     script_path = SCRIPT_DIR.joinpath("get_status.sh")
-    proc = sp.run(shlex.split(f"/bin/bash {str(script_path)} {run_id}"),
-                  stdout=sp.PIPE,
-                  stderr=sp.PIPE,
-                  encoding="utf-8",
-                  env={"SAPPORO_HOST": TEST_HOST,
-                       "SAPPORO_PORT": TEST_PORT})
+    retry = 0
+    while retry < 3:
+        proc = sp.run(shlex.split(f"/bin/bash {str(script_path)} {run_id}"),
+                      stdout=sp.PIPE,
+                      stderr=sp.PIPE,
+                      encoding="utf-8",
+                      env={"SAPPORO_HOST": TEST_HOST,
+                           "SAPPORO_PORT": TEST_PORT})
+        if proc.returncode == 0:
+            break
+        else:
+            retry += 1
     assert proc.returncode == 0
     res_data: RunStatus = json.loads(proc.stdout)
 
