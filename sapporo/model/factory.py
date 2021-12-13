fab7ce1bfd2003b1e169d9d8de3380c165d4fbe6
suecharo <suehiro619@gmail.com>
2021-12-13 17:18:20 +0900

    Fix service info and executable workflows schemas

--- sapporo/model/factory.py
+++ sapporo/model/factory.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 # coding: utf-8
+# pylint: disable=import-outside-toplevel
 import json
 from typing import List
 
 from flask import current_app
-
 from sapporo.model import (Log, RunId, RunListResponse, RunLog, RunStatus,
                            ServiceInfo, Workflow)
-from sapporo.run import (count_system_state, glob_all_run_ids, read_file,
-                         read_state)
 
 
 def generate_service_info() -> ServiceInfo:
+    from sapporo.run import count_system_state
     with current_app.config["SERVICE_INFO"].open(mode="r", encoding="utf-8") as f:
         service_info: ServiceInfo = json.load(f)
 
@@ -32,6 +31,7 @@ def generate_executable_workflows() -> List[Workflow]:
 
 
 def generate_run_status(run_id: str) -> RunStatus:
+    from sapporo.run import read_state
     return {
         "run_id": run_id,
         "state": read_state(run_id)
@@ -39,6 +39,7 @@ def generate_run_status(run_id: str) -> RunStatus:
 
 
 def generate_run_list() -> RunListResponse:
+    from sapporo.run import glob_all_run_ids
     all_run_ids = glob_all_run_ids()
     runs: List[RunStatus] = [generate_run_status(
         run_id) for run_id in all_run_ids]
@@ -50,6 +51,7 @@ def generate_run_list() -> RunListResponse:
 
 
 def generate_run_log(run_id: str) -> RunLog:
+    from sapporo.run import read_file, read_state
     run_log: RunLog = {
         "run_id": run_id,
         "request": read_file(run_id, "run_request"),
@@ -63,6 +65,7 @@ def generate_run_log(run_id: str) -> RunLog:
 
 
 def generate_log(run_id: str) -> Log:
+    from sapporo.run import read_file
     log: Log = {
         "name": read_file(run_id, "run_request")["workflow_name"],
         "cmd": read_file(run_id, "cmd"),
