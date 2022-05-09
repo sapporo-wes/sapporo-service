70ba0629c2db5820efe8e3a9179e2858dcc373be
suecharo <suehiro619@gmail.com>
2022-05-09 00:44:14 +0000

    Update version to 1.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ LONG_DESCRIPTION: Path = BASE_DIR.joinpath("README.md")
 
 setup(
     name="sapporo",
-    version="1.2.0",
+    version="1.2.1",
     description="The sapporo-service is a standard implementation conforming to "
     "the Global Alliance for Genomics and Health (GA4GH) Workflow Execution "
     "Service (WES) API specification.",
