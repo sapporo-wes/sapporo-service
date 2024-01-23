3cd10009de869b20e8241648c21f921adeb090d3
suecharo <suehiro619@gmail.com>
2024-01-23 19:02:46 +0900

    Update scripts under curl_example

--- tests/curl_example/get_run_id.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -Eeu
-
-if [[ $# != 1 ]]; then
-  echo "[Error] Usage: $(basename ${BASH_SOURCE[0]}) run_id"
-  exit 1
-fi
-
-curl -fsSL -X GET http://${SAPPORO_HOST}:${SAPPORO_PORT}/runs/${1}
---
+++ tests/curl_example/nextflow_str_input.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Define default values for SAPPORO_HOST and SAPPORO_PORT
+SAPPORO_HOST=${SAPPORO_HOST:-127.0.0.1}
+SAPPORO_PORT=${SAPPORO_PORT:-1122}
+
+PACKAGE_ROOT="$(cd "$(dirname "$0")" && pwd)"
+while [[ "${PACKAGE_ROOT}" != "/" && ! -f "${PACKAGE_ROOT}/setup.py" ]]; do
+  PACKAGE_ROOT="$(dirname "${PACKAGE_ROOT}")"
+done
+RESOURCE_DIR="${PACKAGE_ROOT}/tests/resources/nextflow"
+
+readonly workflow="${RESOURCE_DIR}/str_input.nf"
+readonly workflow_params='{
+  "str": "sapporo-nextflow-str-input"
+}'
+
+response=$(curl -fsSL -X POST \
+  -H "Content-Type: multipart/form-data" \
+  -F "workflow_params=${workflow_params}" \
+  -F "workflow_type=NFL" \
+  -F "workflow_type_version=1.0" \
+  -F "workflow_url=./str_input.nf" \
+  -F "workflow_engine_name=nextflow" \
+  -F "workflow_attachment=@${workflow}" \
+  http://${SAPPORO_HOST}:${SAPPORO_PORT}/runs)
+
+if [[ $? -ne 0 ]]; then
+  echo -e "Error: Failed to POST runs:\n${response}"
+  exit 1
+fi
+
+run_id=$(echo "${response}" | jq -r '.run_id')
+
+echo -e "POST /runs is succeeded:\n${response}\n"
+echo -e "Please access to the following URL to get the run status:\n"
+echo -e "curl -fsSL -X GET http://${SAPPORO_HOST}:${SAPPORO_PORT}/runs/${run_id}\n"
